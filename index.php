b67e21b7b49d7f05e8ef5ea1634224c348c11f49
ShyamGadde <73636812+ShyamGadde@users.noreply.github.com>
2023-08-09 11:57:09 +0530

    Rename .html files to .php

---
+++ index.php
@@ -0,0 +1,340 @@
+<?php include_once 'includes/header.php'; ?>
+
+
+<header class="pt-5 min-vh-100">
+  <div class="container pt-4 pt-xl-5 d-flex flex-column mt-5">
+    <div class="row pt-2" data-aos="fade-up">
+      <div class="col-md-8 text-center text-md-start mx-auto">
+        <div class="text-center">
+          <h1 class="display-4 fw-bold mb-5" data-aos="fade-up">
+            BookMyDoctor: Your&nbsp;<br />health&nbsp;<span class="underline pb-2">partner</span>&nbsp;online
+          </h1>
+          <p class="fs-5 text-muted mb-5" data-aos="fade-up" data-aos-delay="200">
+            BookMyDoctor lets you book an appointment with your preferred
+            doctor online with little to no hassle. Whether you need a
+            consultation, a check-up, or a treatment, you can find the right
+            doctor for your needs and schedule a visit at your convenience.
+            No more waiting in long queues, no more wasting time on phone
+            calls, no more hassle. Just book, visit, and get well.
+          </p>
+          <div data-aos="fade-up" data-aos-delay="400" class="mt-5">
+            <a class="align-items-center btn btn-primary rounded-pill d-inline-flex align-items-center shadow" href="find-doctor.html">Find a Doctor!&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-chevron-right">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <polyline points="9 6 15 12 9 18"></polyline>
+              </svg></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<section>
+  <div class="container py-4 py-xl-5">
+    <h2 class="text-center" style="font-weight: bold; margin-top: 26px; margin-bottom: 22px">
+      <span class="underline pb-2">Services</span>
+    </h2>
+    <div class="row gy-4 row-cols-1 row-cols-md-2 row-cols-lg-3 justify-content-center" style="text-align: center">
+      <div class="col" data-aos="fade-up">
+        <div class="card border-light border-1 d-flex justify-content-center p-4" style="height: 100%; border-width: 1px; border-radius: 5px">
+          <div class="card-body">
+            <img class="img-fluid rounded-circle mb-3" width="200" height="200" src="assets/img/illustrations/book-appointment.jpg" style="width: 200px; height: 200px" />
+            <div>
+              <h4 class="fw-bold">Online Appointments</h4>
+              <p class="text-muted">
+                Easily schedule appointments with doctors online, avoiding
+                long long waiting times.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col" data-aos="fade-up" data-aos-delay="150">
+        <div class="card border-light border-1 d-flex justify-content-center p-4" style="height: 100%; border-width: 1px; border-radius: 5px">
+          <div class="card-body">
+            <img class="img-fluid rounded-circle mb-3" width="200" height="200" src="assets/img/illustrations/doctor.jpg" style="width: 200px; height: 200px" />
+            <div>
+              <h4 class="fw-bold">Find a Doctor</h4>
+              <p class="text-muted">
+                Effortlessly find the right doctor for your medical need,
+                with our extensive database
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col" data-aos="fade-up" data-aos-delay="300">
+        <div class="card border-light border-1 d-flex justify-content-center p-4" style="height: 100%; border-width: 1px; border-radius: 5px">
+          <div class="card-body">
+            <img class="img-fluid mb-3 rounded-circle" width="218" height="164" src="assets/img/illustrations/medical-record.jpg" style="width: 200px; height: 200px" />
+            <div>
+              <h4 class="fw-bold">24/7 Access to Medical Records</h4>
+              <p class="text-muted">
+                Keep track of your medical history, prescriptions, and lab
+                reports securely with our online medical records management
+                system.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="py-4 py-xl-5">
+  <div class="container" data-aos="fade-left">
+    <div class="bg-primary border rounded border-0 border-primary overflow-hidden">
+      <div class="row g-0">
+        <div class="col">
+          <section class="py-4 py-xl-5">
+            <div class="container h-100">
+              <div class="text-white bg-primary border rounded border-0 p-4 py-5">
+                <div class="row h-100">
+                  <div class="col-md-10 col-xl-8 text-center d-flex d-sm-flex d-md-flex justify-content-center align-items-center mx-auto justify-content-md-start align-items-md-center justify-content-xl-center">
+                    <div>
+                      <h2 class="text-uppercase fw-bold text-white mb-3">
+                        What we stand for
+                      </h2>
+                      <p class="mb-4">
+                        BookMyDoctor is a leading online platform in Pune
+                        that connects patients with doctors and helps them
+                        book appointments conveniently. Our mission is to
+                        provide a seamless and efficient healthcare
+                        experience to patients, reducing wait time and
+                        improving access to quality healthcare services.
+                        Trusted by thousands of patients, BookMyDoctor is
+                        your go-to platform for hassle-free medical
+                        appointments online.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="py-5">
+  <div class="container py-5">
+    <div class="row mb-5">
+      <div class="col-md-8 col-xl-6 text-center mx-auto">
+        <h2 class="fw-bold" data-aos="fade-up">
+          What people say<br /><span class="underline pb-2">about us</span>
+        </h2>
+      </div>
+    </div>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 d-sm-flex justify-content-sm-center">
+      <div class="col mb-4" data-aos="fade-up" data-aos-delay="100">
+        <div class="d-flex align-items-center align-items-sm-start">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-quote fs-1 text-warning flex-shrink-0">
+            <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z"></path>
+          </svg>
+          <div>
+            <p class="fs-6 mb-3 ms-2">
+              Bookmydoctor has made my life easier by allowing me to book
+              doctor appointments online. The convenience of being able to
+              schedule an appointment anytime, anywhere has saved me a lot
+              of time and hassle. The user-friendly interface makes it a
+              breeze to find the right doctor and book an appointment in
+              just a few clicks.
+            </p>
+            <div class="d-flex ms-2">
+              <div class="d-flex">
+                <p class="fw-bold me-2">John Doe</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col mb-4" data-aos="fade-up" data-aos-delay="200">
+        <div class="d-flex align-items-center align-items-sm-start">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-quote fs-1 text-warning flex-shrink-0">
+            <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z"></path>
+          </svg>
+          <div>
+            <p class="fs-6 mb-3 ms-2">
+              I highly recommend Bookmydoctor for anyone looking to schedule
+              doctor appointments online. As a busy professional, I often
+              struggle to find time to make phone calls for appointments.
+              With Bookmydoctor, I can easily find available doctors in my
+              area and book an appointment at my convenience. It's a
+              fantastic service that has simplified the process for me.
+            </p>
+            <div class="d-flex ms-2">
+              <div class="d-flex">
+                <p class="fw-bold me-2">Sam Smith</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col mb-4" data-aos="fade-up" data-aos-delay="300">
+        <div class="d-flex align-items-center align-items-sm-start">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-quote fs-1 text-warning flex-shrink-0">
+            <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z"></path>
+          </svg>
+          <div>
+            <p class="fs-6 mb-3 ms-2">
+              Bookmydoctor has revolutionized the way I schedule my doctor
+              appointments. Gone are the days of waiting on hold or playing
+              phone tag with receptionists. With Bookmydoctor, I can simply
+              search for doctors, view their available time slots, and book
+              an appointment online. It's a time-saving and efficient
+              solution that I can't imagine living without.
+            </p>
+            <div class="d-flex ms-2">
+              <div class="d-flex">
+                <p class="fw-bold me-2">Jane Doe</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="py-5">
+  <div class="container" data-aos="fade-left">
+    <div class="text-white bg-primary border rounded border-primary d-flex flex-column justify-content-between align-items-center flex-lg-row p-4 p-lg-5" data-bs-theme="light">
+      <div class="text-center text-lg-start py-3 py-lg-1">
+        <h2 class="fw-bold mb-2">Subscribe to our newsletter</h2>
+        <p class="mb-0">Imperdiet consectetur dolor.</p>
+      </div>
+      <form class="d-flex justify-content-center flex-wrap flex-lg-nowrap" method="post">
+        <div class="my-2">
+          <input class="shadow-sm form-control" type="email" name="email" placeholder="Your Email" required="" pattern="^[a-zA-Z0-9.!#$%&amp;’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" style="
+                  border-top-left-radius: 5px;
+                  border-bottom-left-radius: 5px;
+                " />
+        </div>
+        <div class="my-2">
+          <button class="btn btn-warning shadow" type="submit" style="
+                  border-top-right-radius: 5px;
+                  border-bottom-right-radius: 5px;
+                ">
+            Subscribe
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</section>
+<section class="py-4 py-xl-5 mb-5">
+  <div class="container" data-aos="fade-up">
+    <div class="row mb-2">
+      <div class="col-md-8 col-xl-6 text-center mx-auto">
+        <h2 class="display-6 fw-bold mb-5">
+          <span class="pb-3 underline">FAQ<br /></span>
+        </h2>
+      </div>
+    </div>
+    <div class="row mb-2">
+      <div class="col-md-8 mx-auto">
+        <div class="accordion text-muted" role="tablist" id="accordion-1">
+          <div class="accordion-item">
+            <h2 class="accordion-header" role="tab">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordion-1 .item-1" aria-expanded="false" aria-controls="accordion-1 .item-1">
+                How can I book a doctor's appointment online?
+              </button>
+            </h2>
+            <div class="accordion-collapse collapse item-1" role="tabpanel" data-bs-parent="#accordion-1">
+              <div class="accordion-body">
+                <p>
+                  To book a doctor's appointment online on Bookmydoctor,
+                  simply visit our website and go to the appointment booking
+                  section. Select your desired doctor, choose a suitable
+                  date and time slot, and provide your contact details. Once
+                  the appointment is successfully booked, you will receive a
+                  confirmation message.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" role="tab">
+              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#accordion-1 .item-2" aria-expanded="true" aria-controls="accordion-1 .item-2">
+                What information do I need to provide for a doctor's
+                appointment booking?
+              </button>
+            </h2>
+            <div class="accordion-collapse collapse show item-2" role="tabpanel" data-bs-parent="#accordion-1">
+              <div class="accordion-body">
+                <p class="mb-0">
+                  When booking a doctor's appointment online, you will need
+                  to provide your name, contact number, preferred doctor,
+                  desired date and time for the appointment, and a brief
+                  description of your health concern. This information helps
+                  us to efficiently schedule your appointment with the
+                  appropriate doctor.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" role="tab">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordion-1 .item-3" aria-expanded="false" aria-controls="accordion-1 .item-3">
+                Can I reschedule or cancel a doctor's appointment booked
+                online?
+              </button>
+            </h2>
+            <div class="accordion-collapse collapse item-3" role="tabpanel" data-bs-parent="#accordion-1">
+              <div class="accordion-body">
+                <p class="mb-0">
+                  Yes, you can reschedule or cancel a doctor's appointment
+                  booked online through Bookmydoctor. Simply login to your
+                  account, go to the appointments section, and select the
+                  appointment you wish to reschedule or cancel. Follow the
+                  instructions provided to make the necessary changes.
+                  Please note that there may be a certain timeframe within
+                  which you can make changes to your appointment.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="py-5">
+  <div class="container py-5" data-aos="fade-up">
+    <div class="row">
+      <div class="col-md-8 col-xl-6 text-center mx-auto">
+        <h2 class="display-6 fw-bold mb-4">
+          Got any <span class="underline">questions</span>?
+        </h2>
+        <p class="text-muted">
+          Our team is always here to help. Send us a message and we'll get
+          back to you shortly.
+        </p>
+      </div>
+    </div>
+    <div class="row d-flex justify-content-center">
+      <div class="col-md-6">
+        <div>
+          <form class="p-3 p-xl-4" method="post" data-bs-theme="light">
+            <div class="mb-3">
+              <input class="shadow form-control" type="text" id="name-1" name="name" placeholder="Name" required="" style="border-radius: 5px" />
+            </div>
+            <div class="mb-3">
+              <input class="shadow form-control" type="email" id="email-1" name="email" placeholder="Email" required="" style="border-radius: 5px" />
+            </div>
+            <div class="mb-3">
+              <textarea class="shadow form-control" id="message-1" name="message" rows="6" placeholder="Message" required="" style="border-radius: 5px"></textarea>
+            </div>
+            <div>
+              <button class="btn btn-primary shadow d-block w-100 rounded-pill" type="submit" style="border-radius: 5px">
+                Send
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<?php include_once "includes/footer.php" ?>
