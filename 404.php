871c9a3c89d8ae98c95770f058c8e973691c0a75
ShyamGadde <73636812+ShyamGadde@users.noreply.github.com>
2023-08-11 21:20:06 +0530

    fix: :lipstick: image too large

--- 404.php
+++ 404.php
@@ -5,13 +5,13 @@ include_once "includes/header.php"
 ?>
 
 
-<section class="py-5 mt-5">
+<section style="margin-top: 80px;">
   <div class="container">
-    <div class="row row-cols-1 d-flex justify-content-center align-items-center">
-      <div class="col-md-10 text-center">
-        <img class="img-fluid w-100" src="assets/img/illustrations/404.svg" />
+    <div class="d-flex flex-column justify-content-center align-items-center">
+      <div class="text-center">
+        <img class="img-fluid" src="assets/img/illustrations/404.svg" />
       </div>
-      <div class="col text-center">
+      <div class="text-center">
         <h2 class="display-3 fw-bold mb-4">Page Not Found</h2>
       </div>
     </div>
